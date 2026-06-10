39e7271118e627146c6aa2a47469e11563dea49e
Kh05ifr4nD <meandSSH0219@gmail.com>
2026-06-10 14:09:50 +0800

    Trim redundant flake checks
    Signed-off-by: Kh05ifr4nD <meandSSH0219@gmail.com>

--- flake/checks.nix
+++ flake/checks.nix
@@ -21,16 +21,12 @@ let
 in
 packages
 // {
-  codexWithoutRipgrep = packages.codex.override { withRipgrep = false; };
-  oxlintWithoutTypecheck = packages.oxlint.override { withTypecheck = false; };
-}
-// lib.optionalAttrs (pkgs.stdenv.hostPlatform.system == "aarch64-linux") {
   mineruWithAll = packages.mineru.override { withAll = true; };
+  oxlintWithoutTypecheck = packages.oxlint.override { withTypecheck = false; };
 }
 // lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
   codexMinimal = packages.codex.override {
     withRipgrep = false;
     withBubblewrap = false;
   };
-  codexWithoutBubblewrap = packages.codex.override { withBubblewrap = false; };
 }
